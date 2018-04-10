5c429007e0c44a99629df86d61f699ba1ff9ff42
Aurora Ruelas <karyna.ruelas@icloud.com>
2018-04-10 09:22:32 +0200

    Primer Commit

---
+++ SegundHito/SegundHito/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegundHito
+//
+//  Created by KARYNA AURORA RUELAS GONZALEZ on 10/4/18.
+//  Copyright © 2018 KARYNA AURORA RUELAS GONZALEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
